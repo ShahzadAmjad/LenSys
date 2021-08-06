862445dff31b69775fd36a376febaba2e20108ec
ShehzadAmjad <shahzadamjad94@gmail.com>
2021-08-06 21:52:06 +0500

    update

---
+++ LenSys/Models/IndividualEmploymentDetails/EmploymentDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LenSys.Models.IndividualEmploymentDetails
+{
+    public class EmploymentDetails
+    {
+        public string EmploymentStatus { get; set; }
+        public string EmploymentType { get; set; }
+        public int GrossSalary { get; set; }
+        public int NationalInsuranceNumber { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime StartDate { get; set; }
+        public string EmployersName { get; set; }
+        public int LatestEarnedIncomeYearOne { get; set; }
+        public int LatestEarnedIncomeYearTwo { get; set; }
+        public int LatestEarnedIncomeYearThree { get; set; }
+        
+    }
+}
