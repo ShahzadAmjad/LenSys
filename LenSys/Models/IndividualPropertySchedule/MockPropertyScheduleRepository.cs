d15466873dde8514ca976649f301535c585a5a20
ShehzadAmjad <shahzadamjad94@gmail.com>
2021-08-09 00:03:48 +0500

    update

---
+++ LenSys/Models/IndividualPropertySchedule/MockPropertyScheduleRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LenSys.Models.IndividualPropertySchedule
+{
+    public class MockPropertyScheduleRepository : IPropertyScheduleRepository
+    {
+        private List<PropertySchedule> _propertySchedule;
+        public MockPropertyScheduleRepository()
+        {
+                
+        }
+        public PropertySchedule Add(PropertySchedule propertySchedule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LenSys/Models/IndividualPropertySchedule/PropertySchedule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LenSys.Models.IndividualPropertySchedule
+{
+    public class PropertySchedule
+    {
+        public string Property { get; set; }
+        public string Owner { get; set; }
+        public string Propertyaddress { get; set; }
+        public string Lender { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int PurchasePrice { get; set; }
+        public int OrigionalMortgageAmount { get; set; }
+        public int CurrentMarketValue { get; set; }
+        public int OutstandingMortgage { get; set; }
+        public int RemainingTerm { get; set; }
+        public int TypeOfRate { get; set; }
+        public int InterestRate { get; set; }
+        public int RentPcm { get; set; }
+        public int MortgagePcm { get; set; }
+        public string LTV { get; set; }
+        public string PropertyType { get; set; }
+        public int PropertyDescription { get; set; }
+        public int LeaseASTBoth { get; set; }
+        public int RemainingTermOfLease { get; set; }
+    }
+}
