df423ba91c71054b0b0207b33cfc37711f8c9a80
ShehzadAmjad <shahzadamjad94@gmail.com>
2021-08-14 00:11:33 +0500

    update

---
+++ LenSys/Models/Home/DocumentList/DocumentList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LenSys.Models.Home.DocumentList
+{
+    public class DocumentList
+    {
+        public int DocumentId { get; set; }
+        public string DocumentNameList { get; set; }
+    }
+}
---
+++ LenSys/Models/Home/Search/Search.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LenSys.Models.Home.Search
+{
+    public class Search
+    {
+        public int SearchId { get; set; }
+        public string SearchAttribute { get; set; }
+        public string SearchParam { get; set; }
+    }
+}
