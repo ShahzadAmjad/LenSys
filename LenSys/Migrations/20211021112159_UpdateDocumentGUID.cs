c8ce99b20a72e98a2cdec839da2111ab8df1969f
Administrator <shahzadamjad94@gmail.com>
2021-10-21 16:26:43 +0500

    update

---
+++ LenSys/Migrations/20211021112159_UpdateDocumentGUID.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LenSys.Migrations
+{
+    public partial class UpdateDocumentGUID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentGuid",
+                table: "IndividualDocuments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentGuid",
+                table: "BusniessDocuments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DocumentGuid",
+                table: "IndividualDocuments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DocumentGuid",
+                table: "BusniessDocuments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
